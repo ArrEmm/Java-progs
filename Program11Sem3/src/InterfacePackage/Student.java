2eb022feab72ab378ea6ad75ee6a4f00bdcebca7
ArrEmm <133852128+ArrEmm@users.noreply.github.com>
2023-05-17 14:46:00 +0530

    Add files via upload

---
+++ Program11Sem3/src/InterfacePackage/Student.java
@@ -0,0 +1,8 @@
+package InterfacePackage;
+
+public interface Student {
+	//final static String name;
+	//final static String branch;
+	void getName();
+	void getBranch();
+}
---
+++ Program11Sem3/src/studentPackage/CreditLimit.java
@@ -0,0 +1,6 @@
+package studentPackage;
+public class CreditLimit extends RuntimeException{
+	public CreditLimit(String msg) {
+		super(msg);
+	}
+}
